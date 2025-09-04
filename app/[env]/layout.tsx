e5279e9496adbc014363bd1e63979a5c5ac7909e
DCarloneUP <daniele.carlone@upsystems.it>
2025-09-04 17:24:02 +0200

    Update layout.tsx

--- app/[env]/layout.tsx
+++ app/[env]/layout.tsx
@@ -1,21 +1,15 @@
 // app/[env]/layout.tsx
 import Sidebar from '@/components/Sidebar';
 import Topbar from '@/components/Topbar';
-import React from 'react';
 
 export default function EnvLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen">
-      {/* Sidebar a larghezza fissa */}
-      <aside className="w-64 shrink-0">
-        <Sidebar />
-      </aside>
-
-      {/* Colonna principale: Topbar + Page */}
-      <div className="flex-1 flex flex-col">
-        <Topbar /> {/* niente position: fixed */}
+    <>
+      <Sidebar />
+      <div className="min-h-screen flex flex-col ml-64">
+        <Topbar />
         <main className="flex-1 px-4 py-6">{children}</main>
       </div>
-    </div>
+    </>
   );
 }
